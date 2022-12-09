9bc48a584afce3f53dac881aee55ebe619f019df
Oksana Robota <orobota@healthjoy.com>
2022-12-09 22:40:15 +0200

    HW4

--- HW4/Homework_4.py
+++ HW4/Homework_4.py
@@ -1,9 +1,9 @@
 variable = input('Please, write your value!\n')
-if variable.replace('.', '').isdigit():
+if variable.isdigit():
     print("It's a number", end=' ')
-    if float(variable) % 2 == 0:
+    if int(variable) % 2 == 0:
         print("and even one as well!")
     else:
         print("and odd one as well!")
 else:
-    print(len(variable))
+    print(f"This is a word! It's length is {len(variable)}")
