0297e6fb9c072d4a450a590d0eeea98a77d8f78a
Oksana Robota <orobota@healthjoy.com>
2022-12-27 11:17:32 +0200

    HW7_update

--- HW7/Homework_7.py
+++ HW7/Homework_7.py
@@ -7,17 +7,20 @@ while True:
         print(*phone_book.keys(), sep='\n')
     elif commands in ['delete', 'show']:
         name = input('Write name:\n')
-        if commands == 'delete':
+        if commands == 'delete' and name in phone_book.keys():
             del phone_book[name]
             print(f'Record with name "{name}" was successfully deleted')
-        elif commands == 'show':
+        elif commands == 'show' and name in phone_book.keys():
             print(f'Here you could see the info for "{name}": {phone_book[name]}')
+        else:
+            print(f'There is no such name as {name} in phone book. Firstly, you need to add it!')
     elif commands == 'add':
         new_name, phone = input('Provide, please, name and phone:\n').split()
         if new_name in phone_book.keys():
             print(f'Record with {new_name} already exists. Please remove it and add a new one after that')
         else:
             phone_book[new_name] = phone
+            print(f'Contact "{new_name}" was created in the phone book!')
     elif commands == 'end':
         print('Thanks for your records. Buy!')
         break
