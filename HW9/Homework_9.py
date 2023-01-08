907891263351d34aea0999819750b974df36ea57
Oksana Robota <orobota@healthjoy.com>
2023-01-08 19:23:24 +0200

    HW9 changes

--- HW9/Homework_9.py
+++ HW9/Homework_9.py
@@ -3,21 +3,13 @@ def fib_gen(n):
     fibo_1, fibo_2 = 0, 1
     for _ in range(1, n + 1):
         fibo_1, fibo_2 = fibo_2, fibo_1 + fibo_2
-        yield fibo_1
+    return fibo_1
+
+
+print(fib_gen(int(input())))
 
 
 # 2. Fibonacci sequences using iterator
-# option a
-def fib_iter(n):
-    d = []
-    fib1, fib2 = 0, 1
-    for _ in range(1, n + 1):
-        fib1, fib2 = fib2, fib1 + fib2
-        d.append(fib1)
-    return iter(d)
-
-
-# option b
 class MyIterator:
 
     def __init__(self, num):
@@ -44,8 +36,14 @@ def fib_rec(n):
     return fib_rec(n - 1) + fib_rec(n - 2)
 
 
+print(fib_rec(int(input())))
+
+
 # 4. Factorial using recursion
 def fact_rec(n):
     if n == 1:
         return 1
     return n * fact_rec(n - 1)
+
+
+print(fact_rec(int(input())))
