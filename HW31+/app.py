51949c5f00cd179c5da9c278318f189e1c28813f
Oksana Robota <orobota@healthjoy.com>
2023-04-10 16:24:34 +0300

    ffff

--- HW31+/app.py
+++
@@ -1,6 +0,0 @@
-from flask_app import app
-
-if __name__ == '__main__':
-    app.run(host=app.config.get('HOST'),
-            port=app.config.get('PORT'),
-            debug=app.config.get('DEBUG'))
