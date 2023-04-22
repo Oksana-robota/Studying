727c6823b9eea296ad7f7fdc6b20925610c2930d
Oksana Robota <orobota@healthjoy.com>
2023-04-22 18:47:02 +0300

    HW40

--- HW38+/book/models.py
+++ HW38+/book/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Book(models.Model):
     title = models.CharField(max_length=255, null=False, unique=True)
     author = models.CharField(max_length=255, null=False, unique=True)
@@ -9,3 +10,9 @@ class Book(models.Model):
     class Meta:
         db_table = 'book'
         unique_together = ('title', 'author')
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+    def __str__(self):
+        return f'''The book {self.title} with id - {self.id} was written by {self.author} in {self.year}.
+                    It's price is {self.price}'''
--- HW38+/user/models.py
+++ HW38+/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class User(models.Model):
     first_name = models.CharField(max_length=255, null=False)
     last_name = models.CharField(max_length=255, null=False)
@@ -7,3 +8,8 @@ class User(models.Model):
 
     class Meta:
         db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f'User with id - {self.id}: {self.first_name} {self.last_name}'
