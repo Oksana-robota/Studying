cc0469c5f5ea54458ffa5502ea9aad01d788ea10
Oksana Robota <orobota@healthjoy.com>
2023-05-06 15:32:30 +0300

    HW42

--- HW38+/book/views.py
+++ HW38+/book/views.py
@@ -4,26 +4,44 @@ from django.views.generic import ListView, DetailView, CreateView
 from .forms import BookForm
 from rest_framework.viewsets import ModelViewSet
 from .serializers import BookSerializer
+import django_filters
+from rest_framework import filters
 
 
-class BookListView(ListView):
-    model = Book
+# class BookListView(ListView):
+#     model = Book
+#
+#
+# class BookDetailView(DetailView):
+#     model = Book
+#     template_name = 'book_detail.html'
+#
+#
+# class BookCreateView(CreateView):
+#     model = Book
+#     form_class = BookForm
 
 
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'book_detail.html'
-
-
-class BookCreateView(CreateView):
-    model = Book
-    form_class = BookForm
-    # success_url = reverse_lazy()
-
+class BookFilter(django_filters.FilterSet):
+    class Meta:
+        model = Book
+        fields = {
+            'title': ['contains'],
+            'price': ['gte', 'lte', 'gt', 'lt', 'exact']
+        }
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filterset_class = BookFilter
+    search_fields = ['title']
+    ordering_fields = ['year']
+    filter_backends = [
+        django_filters.rest_framework.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
 
 # simple views
 # def my_view_book(request):
